cd20cb008d905215e9aa139e3df36176faffb8be
Hyungseop Lee <kgkfdg@gmail.com>
2024-12-01 16:08:39 -0500

    change trackRevenue funcion

--- RDB_FinalProject/trackRevenue.h
+++ RDB_FinalProject/trackRevenue.h
@@ -1,28 +1,33 @@
 #ifndef TRACKREVENUE
 #define TRACKREVENUE
 
-#include <stdio.h>
 #include <mysql.h>
 #include "input.h"
 
+#define QUERYLENGTH 1024
+
+//item id, item name, item quantity, total revenue(sum of total proce)
+//after the purchase happened, sum of specific item and display sum of total_price
 
 void trackRevenue(MYSQL* conn) {
-	/*
-	* Track Revenue from Purchases – Hyungseop
-		Enter Item id
-		Display total sales
-	*/
+
+	MYSQL_RES* res;
+	MYSQL_ROW row;
+
 	int item_id = 0;
 
 	printf("Track Revnue from Purchase\n");
 	getInteger("Enter Item ID: ", &item_id);
 
-	//item id, item name, item quantity, total price
-	//after the purchase happened, sum of specific item and display sum of total_price
 	const char* query = "asasdsd";
 
-	char queryStr[1024];
+	char queryStr[QUERYLENGTH];
 	snprintf(queryStr, sizeof(queryStr), query, item_id);
+
+	if (mysql_query(conn, queryStr)) {
+		fprintf(stderr, "QUERY failed: %s\n", mysql_error(conn));
+		return;
+	}
 
 }
 
