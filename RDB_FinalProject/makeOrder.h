4e7244f9f9c61b423f82a0ad12035ecb684a3657
1syedbil <bsyed7633@conestogac.on.ca>
2024-12-03 13:30:57 -0500

    for some reason makeOrder.h keeps messing with the program when it tries to compile so I just commented out all the code for now.

--- RDB_FinalProject/makeOrder.h
+++ RDB_FinalProject/makeOrder.h
@@ -8,48 +8,48 @@
 
 //-------------------------------------------STRUCTS---------------------------------------------//
 
-typedef struct orderItem {
-	char item_name[MAX_INPUT];
-	int quantity;
-	double item_order_cost;
-	struct orderItem* next;
-} orderItem; 
-
-
-//------------------------------------------PROTOTYPES-------------------------------------------//
-
-void displayOrders() {
-
-}
-
-
-//------------------------------------------MAIN METHOD------------------------------------------//
-
-void createOrder(MYSQL* conn) {
-	bool continueOrder = true;
-	struct orderItem* start = NULL;
-	struct orderItem* current = NULL;
-
-	while (continueOrder) {
-		// prompt user for item name and order quantity
-
-		// query for item
-	}
-}
-
-
-//-----------------------------------------QUERY METHODS-----------------------------------------//
-
-MYSQL_RES* readItem(char* name) {
-
-}
-
-MYSQL_RES* createOrder() {
-
-}
-
-MYSQL_RES* createOrderItem() {
-
-}
+//typedef struct orderItem {
+//	char item_name[MAX_INPUT];
+//	int quantity;
+//	double item_order_cost;
+//	struct orderItem* next;
+//} orderItem; 
+//
+//
+////------------------------------------------PROTOTYPES-------------------------------------------//
+//
+//void displayOrders() {
+//
+//}
+//
+//
+////------------------------------------------MAIN METHOD------------------------------------------//
+//
+//void createOrder(MYSQL* conn) {
+//	bool continueOrder = true;
+//	struct orderItem* start = NULL;
+//	struct orderItem* current = NULL;
+//
+//	while (continueOrder) {
+//		// prompt user for item name and order quantity
+//
+//		// query for item
+//	}
+//}
+//
+//
+////-----------------------------------------QUERY METHODS-----------------------------------------//
+//
+//MYSQL_RES* readItem(char* name) {
+//
+//}
+//
+//MYSQL_RES* createOrder() {
+//
+//}
+//
+//MYSQL_RES* createOrderItem() {
+//
+//}
 
 #endif
